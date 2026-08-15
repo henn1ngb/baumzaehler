2acedcb7ec57cc6754243f19f69a66b67bffcedc
henn1ngb <147529308+henn1ngb@users.noreply.github.com>
2026-08-15 20:09:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Baumzähler – Offline-Cache */
-const CACHE = "baumzaehler-v10";
+const CACHE = "baumzaehler-v11";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
